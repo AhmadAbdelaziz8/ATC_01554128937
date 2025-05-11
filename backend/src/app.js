7d79eb210ff6439b197e2a04b02f21e0c573e9d7
iCobra3 <ahmadabdelaziz8@gmail.com>
2025-05-11 20:23:59 +0300

    add the entry point

---
+++ backend/src/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/api/test", (req, res) => {
+  res.json({ message: "Hello World" });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
